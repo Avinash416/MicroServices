84e0b70ba9534a1add3728ea9b70401f9ce3f39c
AvinashJadhav112 <avinash.j@vervetronics.com>
2025-05-09 18:52:05 +0530

    feat: Add server configuration that can be reusable for other services

---
+++ ServerConfiguration/src/main/java/com/micro/config/ServerConfiguration/ServerConfigurationApplication.java
@@ -0,0 +1,16 @@
+package com.micro.config.ServerConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ServerConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConfigurationApplication.class, args);
+	}
+
+}
---
+++ ServerConfiguration/src/test/java/com/micro/config/ServerConfiguration/ServerConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.config.ServerConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
