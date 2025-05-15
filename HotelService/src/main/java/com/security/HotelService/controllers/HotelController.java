0db9c2972475687d1d5e1abd2ef00b79b8a62832
AvinashJadhav112 <avinash.j@vervetronics.com>
2025-05-15 12:09:01 +0530

    security(HotelService): Add security conf and use preAuthorization in the hotelService controller

--- HotelService/src/main/java/com/security/HotelService/controllers/HotelController.java
+++ HotelService/src/main/java/com/security/HotelService/controllers/HotelController.java
@@ -6,6 +6,7 @@ import com.security.HotelService.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,16 +17,19 @@ public class HotelController {
     private HotelService hotelService;
 
     //get all hotels
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin)")
     @GetMapping
     public ResponseEntity<List<Hotel>> getAll(){
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.getAllHotels());
     }
 
+    @PreAuthorize("hasAuthority('Admin)")
     @PostMapping
     public ResponseEntity<Hotel> create(@RequestBody Hotel hotel){
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> getById(@PathVariable String hotelId){
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotel(hotelId));
