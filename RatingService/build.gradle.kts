ddf319d700de33ae40a0724db4b864a017ffeede
AvinashJadhav112 <avinash.j@vervetronics.com>
2025-05-15 12:06:13 +0530

    chore: Add required dependencies for security and Oauth

--- RatingService/build.gradle.kts
+++ RatingService/build.gradle.kts
@@ -36,6 +36,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.cloud:spring-cloud-starter")
 	implementation ("org.springframework.cloud:spring-cloud-starter-config")
+	implementation ("org.springframework.boot:spring-boot-starter-security")
+	implementation ("com.okta.spring:okta-spring-boot-starter:3.0.7")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client") // 👈 Eureka Client
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
